dbf91838fdd25ac38dcdc2180b510a3b12b05826
Kendell R <KTibow@users.noreply.github.com>
2023-08-01 11:01:07 -0700

    fix some minor stuff

--- proxy/index.js
+++ proxy/index.js
@@ -4,13 +4,13 @@ export default {
    * @returns {Response}
    */
   async fetch({ url, method, headers }) {
+    if (headers.get("origin") == "https://rewatch.to")
+      return new Response("Blocked", { status: 403 });
     if (method == "OPTIONS")
       return new Response("", {
         headers: { "Access-Control-Allow-Origin": "*" },
       });
     if (method == "GET") {
-      if (headers.get("origin") == "https://rewatch.to")
-        return new Response("Blocked", { status: 403 });
       const locationSpecified = new URL(url).searchParams.get("url");
       if (!locationSpecified)
         return new Response("No url to get specified", { status: 400 });
