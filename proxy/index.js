90b0ac12fe2717c1164bfa18841e82a5f0679295
Kendell R <KTibow@users.noreply.github.com>
2023-08-01 10:50:05 -0700

    block these rewatch people

--- proxy/index.js
+++ proxy/index.js
@@ -1,10 +1,19 @@
 export default {
-  async fetch({ url, method }) {
+  /**
+   * @param {Request} request
+   * @returns {Response}
+   */
+  async fetch({ url, method, headers }) {
     if (method == "OPTIONS")
-      return new Response("", { headers: { "Access-Control-Allow-Origin": "*" } });
+      return new Response("", {
+        headers: { "Access-Control-Allow-Origin": "*" },
+      });
     if (method == "GET") {
+      if (headers.get("origin") == "https://rewatch.to")
+        return new Response("Blocked", { status: 403 });
       const locationSpecified = new URL(url).searchParams.get("url");
-      if (!locationSpecified) return new Response("No url to get specified", { status: 400 });
+      if (!locationSpecified)
+        return new Response("No url to get specified", { status: 400 });
       const locationResponse = await fetch(locationSpecified, {
         headers: { "User-Agent": "github.com/SkyblockClient (mart, proxy)" },
       });
